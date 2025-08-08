72701f3ab79f6a31bdc4844b091cf2f7bc59cbc1
shaamiilll <shamilamiyan@gmail.com>
2025-08-08 10:27:38 +0530

    Updated the app.tsx with base

--- src/App.tsx
+++ src/App.tsx
@@ -16,17 +16,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Auranize</h1>
+
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        We are comming soon! Stay tuned for updates.
       </p>
     </>
   )
